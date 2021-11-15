bb26e5ab892e63a24db38ce841ebfe8c9e38cc06
Alex <bugrimov_a@bk.ru>
2021-11-16 01:02:10 +0300

    feat: Added tailwind lib

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import '../App.css';
+import {Banner} from "../components/Banner";
 
 function App() {
   return (
     <div className="App">
-      <h1>Multiplication Random</h1>
+      <Banner/>
     </div>
   );
 }
